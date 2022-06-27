77cad674c05cb1c958c42cd747dcd6b7eba5b87c
Mohammed Mosa <68967059+M4A28@users.noreply.github.com>
2022-06-27 18:43:11 +0200

    Create alerat.sh

---
+++ alerat.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+subject="Server Memory Status Alert"
+##sending mail as
+#from="server.monitor@example.com"
+## sending mail to
+to="**********@gmail.com"
+## send carbon copy to
+#also_to="admin2@example.com"
+
+## get total free memory size in megabytes(MB) 
+free=$(free -mt | grep Total | awk '{print $4}')
+
+## check if free memory is less or equals to  100MB
+if [[ "$free" -le 100  ]]; then
+        ## get top processes consuming system memory and save to temporary file 
+        ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head >/tmp/top_proccesses_consuming_memory.txt
+
+        file=/tmp/top_proccesses_consuming_memory.txt
+        ## send email if system memory is running low
+        echo -e "Warning, server memory is running low!\n\nFree memory: $free MB" | mutt -a "$file" -s "$subject"  -c "$to"
+fi
+
+exit 0
